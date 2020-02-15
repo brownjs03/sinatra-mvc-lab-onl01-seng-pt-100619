74db5a5f4746c913c5d20435ed0f9568d8a9d3d5
Jeffrey Brown <brownjs03@gmail.com>
2020-02-15 19:50:17 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -23,7 +23,7 @@ class PigLatinizer
   end
   
   def piglatinize_sentence(sentence)
-    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+    sentence.split.collect { |word| piglatinize(word) }.join(" ")
   end
   
   
