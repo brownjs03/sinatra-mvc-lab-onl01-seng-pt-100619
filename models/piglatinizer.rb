a945d06f8430e52eb76466a582dfb2d6d19feae7
Jeffrey Brown <brownjs03@gmail.com>
2020-02-15 19:52:28 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,7 +4,9 @@ class PigLatinizer
      
   end 
   
-  
+  def piglatinize(input_str)
+    input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+  end
 
   def consonant?(char)
     !char.match(/[aAeEiIoOuU]/)
